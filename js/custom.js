a1285a0f97f3038c47ec5333e0086c9d51de98af
Kevin Luo <kevinluo201@gmail.com>
2018-03-17 18:02:27 +0800

    merge with mooncake's 3/14's modification

--- js/custom.js
+++ js/custom.js
@@ -8,8 +8,22 @@ function nav(){
     });
 }
 
+function album(){
+	$(".small img").click( function(){
+		var NN = $(this).index();
+		$(".banner img").eq(NN).fadeIn(800).siblings().fadeOut(800);
+	});
+}
+
+function carousel(){
+    $(".carousel").carousel();
+    $(".carousel02").carousel();
+}
+
 $(document).ready(function(){
     nav();
+    album();
+    carousel();
 });
 
 
